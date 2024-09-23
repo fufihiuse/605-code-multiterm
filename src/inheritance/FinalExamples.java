52c2e8de87761475ee24f9bdc7c005e59d82d416
eduardocoelholima <eduardocoelholima@gmail.com>
2024-09-23 09:00:03 -0400

    sync

---
+++ src/inheritance/FinalExamples.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class FinalExamples {
+    static final int number;
+    static final int someOtherNumber = 33;
+    final int instanceNumber;
+    final int someOtherInstanceNumber = 333;
+
+    static { number = 2;}
+
+    FinalExamples() {
+        instanceNumber = 3;
+    }
+
+
+
+}
