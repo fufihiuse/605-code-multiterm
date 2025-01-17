37b9c9590bac1b441680b696d336e88303a82cd2
eduardocoelholima <eduardocoelholima@gmail.com>
2025-01-17 09:46:43 -0500

    update

--- hw/hw01/provided_files/problems/Problem2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Problem2 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int i = in.next();
-		System.out.println(i);
-	}
-}
--- hw/hw01/provided_files/problems/Problem9.java
+++
@@ -1,5 +0,0 @@
-public class Problem9 {
-	public static void main(String[] args) {
-		float sum = 0.0;
-	}
-}
