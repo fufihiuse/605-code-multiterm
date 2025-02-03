f2ce4058535fde4369322f605ffe90714871dd4d
eduardocoelholima <eduardocoelholima@gmail.com>
2025-02-03 13:57:19 -0500

    update

--- hw/hw01/provided_files/problems/Problem3.java
+++
@@ -1,5 +0,0 @@
-public class Problem3 {
-	public static void main(String[] args) {
-		int i = 5 / 0;
-	}
-}
--- hw/hw01/provided_files/problems/Problem6.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Problem6 {
-	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<Integer>(5);
-		int i = list.get(0);
-	}
-}
